715ecb396714e783afed8ab06ea703bd3167b735
John Carlee <jcarlee@gmail.com>
2019-02-08 01:19:55 -0600

    simple implementation works

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import PyPDF2
 import sqlite3
-from sqlite3 import Error
-
-import sqlite3
-from sqlite3 import Error
+import re
 
 conn = sqlite3.connect("C:\\Users\\jcarl\\PycharmProjects\\pdf-invoice-scraper\\invoice.db")
 c = conn.cursor()
@@ -46,15 +43,50 @@ for index, line in enumerate(short_list):
 mark_first = markers[:-1]
 mark_last = markers[1:]
 
+
 for x, y in zip(mark_first, mark_last):
-    for i in short_list[x:y]:
-        if y-x == 5:
-            pass
-        elif y-x == 6:
-            pass
+    cur_list = short_list[x:y]
+    if y-x == 5:
+        qty = cur_list[0]
+        itm = cur_list[1]
+        prc = cur_list[2].split()
+        price = prc[0].replace('$', '')
+        item_type = prc[1]
+        price_total = cur_list[3].replace('$', '').strip()
+        name_list = list(filter(None, cur_list[4].split('  ')))
+        item_long = name_list[0]
+        rep = {" ST": "", " BU": ""}
+        rep = dict((re.escape(k), v) for k, v in rep.items())
+        pattern = re.compile("|".join(rep.keys()))
+        item = pattern.sub(lambda m: rep[re.escape(m.group(0))], item_long)
+        sql = ''' INSERT INTO test(invoice,date,source,qty,itm,item,type,price,price_total)
+        VALUES('{0}', '{1}', '{2}', {3}, '{4}', '{5}', '{6}', {7}, {8}) '''.format(invoice_no, invoice_date, 'Krueger',
+                                                                                   qty, itm, item, item_type, price,
+                                                                                   price_total)
+        c.execute(sql)
 
+    elif y-x == 6:
+        qty = cur_list[0]
+        itm = cur_list[1]
+        prc = cur_list[2].split()
+        price = prc[0].replace('$', '')
+        item_type = prc[1]
+        price_total = cur_list[3].replace('$', '').strip()
+        name_list = list(filter(None, cur_list[4].split('  ')))
+        item_long = name_list[0]
+        rep = {" ST": "", " BU": ""}  # Consolidate into function
+        rep = dict((re.escape(k), v) for k, v in rep.items())
+        pattern = re.compile("|".join(rep.keys()))
+        item = pattern.sub(lambda m: rep[re.escape(m.group(0))], item_long)
+        desc = cur_list[5]
+        sql = r'''INSERT INTO test(invoice, date, source, qty, itm, item, type, price, price_total, desc)
+        VALUES('{0}', '{1}', '{2}', {3}, '{4}', '{5}', '{6}', {7}, {8}, '{9}')'''.format(invoice_no, invoice_date,
+                                                                                        'Krueger', qty, itm, item,
+                                                                                        item_type, price, price_total,
+                                                                                        desc)
+        c.execute(sql)
 
-# Loop through line items
-# Loop SQL statements for line items
-# Extract freight
 pdfFileObj.close()
+conn.commit()
+conn.close()
+# Extract freight
